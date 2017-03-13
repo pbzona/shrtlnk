0d71215424de19a6e6c9741683b9c8c737baef76
pbzona <phil.b.zona@gmail.com>
2017-03-12 22:11:02 -0400

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Router, Route, browserHistory} from 'react-router';
+
+import {Meteor} from 'meteor/meteor';
+
+import Login from './../imports/ui/Login';
+import Signup from './../imports/ui/Signup';
+import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Login}/>
+    <Route path="/signup" component={Signup}/>
+    <Route path="/links" component={Link}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+);
+
+Meteor.startup(() => {
+  ReactDOM.render(routes, document.getElementById('app'));
+});
