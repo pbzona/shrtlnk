7023b4713fb24e8aaf87d3d3b5bae2ff0f1ae8cd
Phil Zona <pzona@linode.com>
2017-03-13 16:54:57 -0400

    Add signup functionality

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import {Link} from 'react-router';
+import {Accounts} from 'meteor/accounts-base';
 
 export default class Signup extends React.Component {
   constructor(props) {
@@ -13,6 +13,16 @@ export default class Signup extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Accounts.createUser({
+      email,
+      password
+    }, (err) => {
+      console.log('Signup callback', err)
+    });
 
     this.setState({
       error: 'Something went wrong.'
@@ -27,8 +37,8 @@ export default class Signup extends React.Component {
         {this.state.error ? <p>{this.state.error}</p> : undefined}
 
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="email" name="email" placeholder="Email"/>
-          <input type="password" name="password" placeholder="Password"/>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="password" ref="password" name="password" placeholder="Password"/>
           <button>Create Account</button>
         </form>
 
