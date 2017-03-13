0e300071ce066c2b4922b2fda3e7a1e20971915f
Phil Zona <pzona@linode.com>
2017-03-13 17:22:43 -0400

    Add login functionality

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,16 +1,41 @@
 import React from 'react';
-
-import {Link} from 'react-router'
+import {Link} from 'react-router';
+import {Meteor} from 'meteor/meteor';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: ''
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({email}, password, (err) => {
+      console.log('Login callback', err)
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Log in to ShrtLnk</h1>
+        <h1>ShrtLnk</h1>
 
-        login form here
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-        <Link to="/signup">Create an account</Link>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="password" ref="password" name="password" placeholder="Password"/>
+          <button>Log In</button>
+        </form>
+
+        <Link to="/signup">Create a new account</Link>
       </div>
     );
   }
