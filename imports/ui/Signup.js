dae48f12be9c58037954e00554b2a991848c6abc
pbzona <phil.b.zona@gmail.com>
2017-03-14 20:34:32 -0400

    Even better signup validation

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -17,19 +17,19 @@ export default class Signup extends React.Component {
     let email = this.refs.email.value.trim();
     let password = this.refs.password.value.trim();
 
-    if (password.length > 7) {
-      Accounts.createUser({email, password}, (err) => {
-        if (err) {
-          this.setState({error: err.reason});
-        } else {
-          this.setState({error: ''});
-        }
-      });
-    } else {
-      this.setState({
+    if (password.length < 8) {
+      return this.setState({
         error: 'Password must be at least 8 characters long'
       });
     }
+
+    Accounts.createUser({email, password}, (err) => {
+      if (err) {
+        this.setState({error: err.reason});
+      } else {
+        this.setState({error: ''});
+      }
+    });
   }
 
   render() {
