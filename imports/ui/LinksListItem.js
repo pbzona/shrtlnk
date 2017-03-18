b9d9b1d13272ddefc9c98c055bfdd7afe6e22c2d
pbzona <phil.b.zona@gmail.com>
2017-03-18 17:15:51 -0400

    Interactive copy button

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,11 +2,26 @@ import React from 'react';
 import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			justCopied: false
+		};
+	}
+
 	componentDidMount() {
 		this.clipboard = new Clipboard(this.refs.copy);
 
 		this.clipboard.on('success', () => {
-			console.log('it worked!')
+			this.setState({
+				justCopied: true
+			});
+
+			setTimeout(() => {
+				this.setState({
+					justCopied: false
+				});
+			}, 500);
 		}).on('error', () => {
 			alert('Browser doesn\'t support copying. Please copy the link manually.')
 		});
@@ -21,7 +36,9 @@ export default class LinksListItem extends React.Component {
 			<div>
 				<p><strong>{this.props.shortUrl}</strong></p>
 				<p>{this.props.url}</p>
-				<button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+				<button ref="copy" data-clipboard-text={this.props.shortUrl}>
+					{this.state.justCopied ? 'Copied!' : 'Copy'}
+				</button>
 			</div>
 		);
 	}
