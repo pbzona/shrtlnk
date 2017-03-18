8d2fa73bc3be3ef7e281143c8f21a63eb6c416bf
pbzona <phil.b.zona@gmail.com>
2017-03-18 19:42:04 -0400

    Add tracking for link views

--- server/main.js
+++ server/main.js
@@ -1,4 +1,4 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import {WebApp} from 'meteor/webapp';
 
 import '../imports/api/users';
@@ -15,6 +15,7 @@ Meteor.startup(() => {
 			res.statusCode = 302;
 			res.setHeader('Location', link.url);
 			res.end();
+			Meteor.call('links.trackVisit', _id);
 		} else {
 			next();
 		}
