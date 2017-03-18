fa57a66242a4511b81f17c284fe2ed84c5596264
pbzona <phil.b.zona@gmail.com>
2017-03-18 16:57:39 -0400

    Added copy to clipboard

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+	componentDidMount() {
+		this.clipboard = new Clipboard(this.refs.copy);
+
+		this.clipboard.on('success', () => {
+			console.log('it worked!')
+		}).on('error', () => {
+			alert('Browser doesn\'t support copying. Please copy the link manually.')
+		});
+	}
+
+	componentWillUnmount() {
+		this.clipboard.destroy();
+	}
+
 	render() {
 		return (
 			<div>
 				<p><strong>{this.props.shortUrl}</strong></p>
 				<p>{this.props.url}</p>
+				<button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
 			</div>
 		);
 	}
